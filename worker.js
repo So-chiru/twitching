76254c47381b9c2d06d221613bbd2a119cbdc23a
So-chiru <unleash9224@gmail.com>
2022-10-01 08:55:42 +0900

    Add worker.js

---
+++ worker.js
@@ -0,0 +1,21 @@
+addEventListener('fetch', (event) =>
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  )
+)
+
+async function handleRequest(request) {
+  const parsed = new URL(request.url)
+  let response = await fetch(
+    `https://usher.ttvnw.net/api/channel/hls` +
+      parsed.href.replace(parsed.origin, ''),
+    {
+      headers: request.headers,
+    }
+  )
+  response = new Response(response.body, response)
+  response.headers.set('Access-Control-Allow-Origin', '*')
+  return response
+}
