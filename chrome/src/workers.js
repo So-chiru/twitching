8faf985277126351a0e2483198a080ec42e93956
KunHee Cho <jokuna.dev@gmail.com>
2023-01-18 18:00:33 +0900

    chrome google proxy

--- chrome/src/workers.js
+++ chrome/src/workers.js
@@ -3,7 +3,7 @@ chrome.runtime.onInstalled.addListener(details => {
     chrome.storage.local.set({ server: 'workers' })
 
     chrome.declarativeNetRequest.updateDynamicRules({
-      removeRuleIds: [1],
+      removeRuleIds: [1, 2, 3],
       addRules: [
         {
           id: 1,
@@ -26,6 +26,35 @@ chrome.runtime.onInstalled.addListener(details => {
               'webtransport',
               'webbundle',
               'other',
+            ],
+          },
+        },
+        {
+          id: 2,
+          priority: 1,
+          action: {
+            type: 'modifyHeaders',
+            requestHeaders: [
+              { "header": "referer", "operation": "set", "value": "https://www.twitch.tv/" },
+            ],
+          },
+          condition: {
+            regexFilter: '^https://www-opensocial.googleusercontent.com/',
+          },
+        },
+        {
+          id: 3,
+          priority: 1,
+          action: {
+            type: 'redirect',
+            redirect: {
+              regexSubstitution: 'https://www-opensocial.googleusercontent.com/gadgets/proxy?container=focus&url=\\0',
+            },
+          },
+          condition: {
+            regexFilter: '^https:\/\/video-edge-[A-z0-9]+\.[A-z0-9]+\.abs\.hls\.ttvnw\.net/(.*)',
+            requestMethods: [
+              'get'
             ],
           },
         },
